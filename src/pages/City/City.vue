7610cfee743f894946ea1cb7ea0b2598343667f1
nbadota <383973155@qq.com>
2020-09-26 18:18:20 +0800

    add cityrouter

---
+++ src/pages/City/City.vue
@@ -0,0 +1,17 @@
+<template>
+    <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/Header';
+export default {
+  name: 'City.vue',
+  components: {
+    CityHeader
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,10 +6,12 @@
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -53,6 +55,7 @@ export default {
   width: 1.24rem;
   float: right;
   text-align: center;
+  color: #fff;
 }
 .arrow-icon {
   font-size: .24rem;
