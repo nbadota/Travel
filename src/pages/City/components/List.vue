b6fe3bfc10c349641cf2dbe89334d564aab31a7b
nbadota <383973155@qq.com>
2020-09-27 17:37:34 +0800

    perfomance

--- src/pages/City/components/List.vue
+++ src/pages/City/components/List.vue
@@ -17,7 +17,7 @@
             </div>
           </div>
         </div>
-        <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
           <div class="title border-topbottom">{{key}}</div>
           <div class="item-list">
             <div class="item border-bottom" v-for="innerItem of item"
@@ -34,13 +34,22 @@ export default {
   name: 'Citylist',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper);
   },
   updated () {
     this.scroll.refresh();
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0];
+        this.scroll.scrollToElement(element);
+      }
+    }
   }
 };
 </script>
